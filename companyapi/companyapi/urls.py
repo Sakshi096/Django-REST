069f540023a5802c1728abc9a8558714c58b1f96
Sakshi096 <sakshi.t0120@gmail.com>
2024-06-08 09:37:37 +0530

    models, views and serializers

--- companyapi/companyapi/urls.py
+++ companyapi/companyapi/urls.py
@@ -17,13 +17,10 @@ Including another URLconf
 
 
 
-
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from .views import home_page
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("home/", home_page),
-    path("api/v1/",  include('api.urls'))
+    path('admin/', admin.site.urls),
+    path("home/",home_page),
+    path("api/v1/",include('api.urls'))
 ]
--- companyapi/companyapi/views.py
+++ companyapi/companyapi/views.py
@@ -1,12 +1,14 @@
 
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse,JsonResponse
 
-def home_page(requests):
-    print('home page requested')
-    friends = ['Ankit', 'Sakshi', 'Gupta', 'Siri']
-    # return HttpResponse("</h1>THIS  IS THE Home Page</h1>")
-    return JsonResponse(friends, safe = False)
-
+def home_page(request):
+    print("home page requested")
+    friends=[
+        'ankit',
+        'ravi',
+        'uttam'
+    ]
+    return JsonResponse(friends,safe=False)
 
 
